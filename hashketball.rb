e162d0158babb460b56d87d451b191afea51c7de
Laura Berge <lauraeberge@gmail.com>
2019-08-31 04:40:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -281,4 +281,20 @@ def most_points_scored
   end
 end
 
-
+def player_with_longest_name
+  name_length = 0
+  game_data = game_hash
+  
+  game_data.each do |location, info| 
+    info.each do |attribute, data| 
+      if attribute == :players 
+        data.each do |player_name, stats|
+          if stats[:points] > most_points
+            most_points = stats[:points]
+          end
+        end
+      end
+    end
+  end
+  
+end
